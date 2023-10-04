264f985efbc0b0e2fe15afac09c8a8557a975456
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2023-10-04 21:23:00 +0530

    Add User login/registeration code

---
+++ main.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Change this to a random secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(100))
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return 'Welcome to the User Management App'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        username = request.form['username']
+        password = request.form['password']
+
+        hashed_password = generate_password_hash(password, method='sha256')
+        new_user = User(first_name=first_name, last_name=last_name, username=username, password=hashed_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('User registered successfully!', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login failed. Please check your credentials.', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' in session:
+        user = User.query.filter_by(id=session['user_id']).first()
+        return f'Welcome, {user.first_name} {user.last_name}! This is your dashboard.'
+    else:
+        flash('You need to login first.', 'danger')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('Logged out successfully!', 'success')
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
