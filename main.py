044f2b7febb72ceb5405854c72227bc50f962f61
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2023-10-15 00:56:17 +0530

    added logic for good/bad user and render page

--- main.py
+++ main.py
@@ -2,18 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from passlib.hash import pbkdf2_sha256 as sha256
+from user_payments import get_column_as_list, good_bad
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Change this to a random secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_database.db'
 db = SQLAlchemy(app)
 
-emi_data = [
-    {"due_date": "2023-10-15", "payment_date": "2023-10-12"},
-    {"due_date": "2023-09-15", "payment_date": "2023-09-18"},
-    {"due_date": "2023-08-15", "payment_date": "2023-08-15"},
-    # Add more data as needed
-]
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,9 +26,29 @@ def home():
     return render_template("index.html")
     # return 'Welcome to the User Management App'
 
-@app.route('/payment_history')
+@app.route('/payment_history', methods=['GET', 'POST'])
 def display_emi_history():
-    return render_template('payment_page.html', emi_data=emi_data)
+
+    if 'user_id' in session:
+        user = User.query.filter_by(id=session['user_id']).first()
+
+        due_dates = get_column_as_list(user.username, 'due_date')
+        payment_dates = get_column_as_list(user.username, 'payment_date')
+        agreement_numbers = get_column_as_list(user.username, 'agreement_number')
+
+        payment_data = []
+
+        # Create a list of dictionaries
+        for due_date, payment_date in zip(due_dates, payment_dates):
+            date_dict = {"due_date": due_date, "payment_date": payment_date}
+            payment_data.append(date_dict)
+
+        return render_template('payment_page.html', emi_data=payment_data)
+    else:
+        flash('You need to login first.', 'danger')
+        return redirect(url_for('login'))
+
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -77,6 +92,12 @@ def login():
 def dashboard():
     if 'user_id' in session:
         user = User.query.filter_by(id=session['user_id']).first()
+
+        if good_bad(user.username):
+            return render_template('good_user.html');
+        else:
+            return render_template('bad_user.html');
+
         return f'Welcome, {user.first_name} {user.last_name}! This is your dashboard.'
     else:
         flash('You need to login first.', 'danger')
